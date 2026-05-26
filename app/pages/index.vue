560e30e4536459b08dc3dd9fc0056dcb638ad10b
Jalu Wibowo Aji <jaluwibowoaji@gmail.com>
2026-05-26 19:43:30 +0700

    fix: content not scrolling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -139,7 +139,7 @@
         />
       </div>
 
-      <div class="flex-1 overflow-y-auto p-3 space-y-2">
+      <div class="flex-1 min-h-0 overflow-y-auto p-3 space-y-2">
         <template v-if="pending">
           <div
             v-for="n in 5"
