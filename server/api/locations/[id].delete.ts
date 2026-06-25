8c2d2a7b86dcb83a75be747f1877bbdd2afc4bc3
Jalu Wibowo <jaluwibowoaji@gmail.com>
2026-06-25 18:00:13 +0700

    feat: add notif when edit/delete

--- server/api/locations/[id].delete.ts
+++ server/api/locations/[id].delete.ts
@@ -1,11 +1,23 @@
-import { softDeleteLocation } from '../../utils/sheets'
+import { getLocations, softDeleteLocation } from '../../utils/sheets'
 import { requireAdmin } from '../../utils/admin'
+import { sendLocationDeletedNotification } from '../../utils/mailer'
 
 export default defineEventHandler(async (event) => {
   const email = await requireAdmin(event)
   const id = getRouterParam(event, 'id')
   if (!id) throw createError({ statusCode: 400, message: 'ID is required' })
 
+  const all = await getLocations()
+  const current = all.find((l) => l.id === id)
+
   await softDeleteLocation(id, email)
+
+  if (current?.creator && current.creator !== email) {
+    await sendLocationDeletedNotification({
+      name: current.name,
+      creator: current.creator,
+    }).catch((err) => console.error('[mailer] Failed to send deletion notification:', err))
+  }
+
   return { success: true }
 })
